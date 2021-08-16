31e4002c6d750661b597f3a76427f0f71ae8e921
Mikhail Shilov <mih.shilov@gmail.com>
2021-08-17 01:37:12 +0300

    More improved design Task components

--- client/components/Todo/Task.js
+++ client/components/Todo/Task.js
@@ -13,17 +13,16 @@ const Task = (props) => {
     setTitleValue(event.target.value)
   }
 
-
   const ButtonEdit = () => (
     <button
       type="button"
-      className="flex-no-shrink p-2 ml-4 mr-1 border-2 w-20 rounded hover:text-white text-green border-green hover:bg-green"
+      className="flex-shrink-0 text-sm px-1 py-1 my-1 mx-1 border-2 w-16	rounded hover:text-gray-400 text-green border-green hover:bg-yellow-500"
       onClick={() => { setEditMode(true) }}>Edit</button>)
 
   const ButtonSave = () => (
     <button
       type="button"
-      className="flex-no-shrink p-2 ml-4 mr-1 border-2 w-20 rounded hover:text-white text-green border-green hover:bg-green"
+      className="flex-shrink-0 text-sm	px-1 py-1 my-1 mx-1 border-2 w-16	rounded hover:text-gray-400 text-green border-green hover:bg-yellow-500"
       onClick={() => {
         doPatchTitle(taskId, titleValue)
         setEditMode(false)
@@ -32,31 +31,31 @@ const Task = (props) => {
   const ButtonInProgress = () => (
     <button
       type="button"
-      className="flex-no-shrink p-2 ml-2 mr-1 border-2 w-28 rounded hover:text-white text-green border-green hover:bg-green"
+      className="flex-no-shrink px-2 py-1 ml-2 mr-1 border-2 w-28 rounded hover:text-gray-400 text-green border-green hover:bg-yellow-500"
       onClick={() => { doPatchStatus(taskId, INPROGRESS) }}>In progress</button>)
 
   const ButtonBlocked = () => (
     <button
       type="button"
-      className="flex-no-shrink p-2 ml-2 mr-1 border-2 w-28 rounded hover:text-white text-green border-green hover:bg-green"
+      className="flex-no-shrink px-2 py-1 ml-2 mr-1 border-2 w-28 rounded hover:text-gray-400 text-green border-green hover:bg-yellow-500"
       onClick={() => { doPatchStatus(taskId, BLOCKED) }}>Block</button>)
 
   const ButtonResume = () => (
     <button
       type="button"
-      className="flex-no-shrink p-2 ml-2 mr-1 border-2 w-28 rounded hover:text-white text-green border-green hover:bg-green"
+      className="flex-no-shrink px-2 py-1 ml-2 mr-1 border-2 w-28 rounded hover:text-gray-400 text-green border-green hover:bg-yellow-500"
       onClick={() => { doPatchStatus(taskId, INPROGRESS) }}>Resume</button>)
 
   const ButtonDone = () => (
     <button
       type="button"
-      className="flex-no-shrink p-2 ml-2 mr-1 border-2 w-28 rounded hover:text-white text-green border-green hover:bg-green"
+      className="flex-no-shrink px-2 py-1 ml-2 mr-1 border-2 w-28 rounded hover:text-gray-400 text-green border-green hover:bg-yellow-500"
       onClick={() => { doPatchStatus(taskId, DONE) }}>Done</button>)
 
   const ButtonRemove = () => (
     <button
       type="button"
-      className="flex-no-shrink p-2 ml-2 mr-1 border-2 w-28 rounded hover:text-white text-green border-green hover:bg-green"
+      className="flex-no-shrink px-2 py-1 ml-2 mr-1 border-2 w-28 rounded hover:text-gray-400 text-green border-green hover:bg-yellow-500"
       onClick={() => { doDelete(taskId) }}>Remove</button>)
 
   // const Title = () => (<a onClick={() => { setEditMode(true) }}>{title}</a>)
@@ -65,16 +64,23 @@ const Task = (props) => {
     <input
       name="titleEditor"
       type='text'
-      className="shadow appearance-none border rounded py-2 px-3 mr-4 text-grey-darker"
+      className="shadow flex-grow appearance-none border rounded py-2 px-3 mr-4 text-grey-darker"
       value={titleValue}
       onChange={editHandler}
     />
   )
 
+  const Title = () => (
+    <a className='flex-grow justify-center'>
+      {title}
+    </a>
+
+  )
+
   return (
-    <div className="flex flex-col gap-4 my-px px-6 py-2 items-center bg-indigo-50	">
-      <div>
-        {editMode ? <>{EditTitle()} <ButtonSave /></> : <>{title} <ButtonEdit /></>}
+    <div className="flex flex-col gap-1.5 my-px p-2  items-center bg-indigo-50	">
+      <div className="flex justify-center items-center border w-full p-2">
+        {editMode ? <>{EditTitle()} <ButtonSave /></> : <>{Title()} <ButtonEdit /></>}
       </div>
       <div>
         {(status === 'new') && <ButtonInProgress />}
